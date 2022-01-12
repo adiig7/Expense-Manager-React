3364d2e5731f0a6dccbd448c1813bd00880d8eb1
Aditya Gupta <adutyagupta765469@gmail.com>
2022-01-12 21:29:48 +0530

    new commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
 //Initial State
 const initialState = {
@@ -16,4 +17,9 @@ export const GlobalContext = createContext(initialState);
 // Provider component
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>)
 }
